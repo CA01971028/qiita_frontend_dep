dc783a5686e840a396dcb84e17f04b6cb3d8e735
CA01971028 <107534406+CA01971028@users.noreply.github.com>
2024-11-14 10:40:32 +0900

    修正

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import {useState } from "react";
 import { handleClick } from "../utils/handleclick";
 export default function Card(props) {
-  const { card_id, title, description, tags, score, date ,user} = props; // プロパティを受け取る
+  const {title, tags, score, date ,user} = props; // プロパティを受け取る
   const [liked, setLiked] = useState(false);
  
   return (
@@ -31,41 +31,6 @@ export default function Card(props) {
                 </p>
             </div>
         </div>
-      
-
-
-
-
-
-      {/* <div className="max-w-sm w-full lg:max-w-full lg:flex my-8 "> */}
-        {/* <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" title={title}></div> */}
-        {/* カードの大枠と背景の指定 */}
-        {/* <div className="border border-gray-400 p-4 flex flex-col justify-between leading-normal bg-teal-100">
-          <div className="mb-2">
-            <div className="flex items-center">
-              <img className="w-10 h-10 rounded-full mr-4" src="共食いタコ.png" alt="Avatar"/>
-              <div className="text-sm">
-                <p className="text-gray-900 leading-none">ユーザー名</p> 
-                <p className="text-gray-600">{date}</p>
-              </div>
-            </div>
-            <div className="px-6 pt-4 pb-2 text-gray-900 font-bold text-xl mb-2">
-              {title}
-            </div>
-            <div className="px-6 pt-4 ">
-              {tags.map((tag, index) => (
-                <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{tag}</span>
-              ))}
-              <p className="">
-                <button onClick={() => handleClick(liked, setLiked)}>
-                  <span className={`i-heroicons-solid-heart w-10 h-10 ${liked ? "" : "text-red-500"}`}></span>
-                  {liked ? `${score}` : `${score + 1}`}
-                </button>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div> */}
     </>
     
   );
