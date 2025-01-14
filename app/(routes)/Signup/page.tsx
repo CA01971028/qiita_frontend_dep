349c33902ba9428f7437bd5e9b9115a4548c45bc
CA01971028 <CA01971028@st.kawahara.ac.jp>
2025-01-14 11:50:27 +0900

    サインアップ機能を搭載

---
+++ app/(routes)/Signup/page.tsx
@@ -0,0 +1,16 @@
+import Signup from "./SignUp";
+
+
+
+const page = () => {
+    return (
+        <div className="flex flex-col md:flex-row items-center justify-center w-full min-h-screen bg-gray-100">
+            <div className="bg-white shadow-md rounded-lg p-8 max-w-sm w-full">
+                <h1 className="text-2xl font-bold text-center mb-6">Sign Up</h1>
+                <Signup />
+            </div>
+        </div>
+    );
+}
+
+export default page;
